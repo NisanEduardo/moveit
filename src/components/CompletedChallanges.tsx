c7603b3c9c00e5aed79ec9bf9721fd9b652dfabb
Eduardo Nisan <eduardo.nisan@agencian1.com.br>
2021-03-05 21:40:07 -0300

    primeiro commit

---
+++ src/components/CompletedChallanges.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/CompletedChallangers.module.css'
+
+export default function CompletedChallangers() {
+    return (
+        <div className={ styles.completedChallangersContainer } >
+            <span>Desafios completos</span>
+            <span>5</span>
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import CompletedChallangers from "../components/CompletedChallanges";
+import { Countdown } from "../components/Countdown";
+import ExperienceBar from "../components/ExperienceBar";
+import Profile from "../components/Profile";
+
+import styles from '../styles/pages/Home.module.css'
+
+export default function Home() {
+    return (
+        <div className={ styles.container }>    
+            <ExperienceBar/>
+
+            <section>
+                <div>
+                    <Profile />
+                    <CompletedChallangers />
+                    <Countdown />
+                </div>
+
+                <div>
+
+                </div>
+            </section>
+        </div>
+    )
+}
